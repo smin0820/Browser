747c56d23d08a29fd39b341b5cef80c02a2f9777
신민 <smin_0820@naver.com>
2023-02-03 17:39:10 +0900

    animation 성능 개선
    top, left -> transform

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,14 +3,20 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const coordinate = document.querySelector('.coordinate');
 
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    coordinate.style.top = `${y}px`;
-    coordinate.style.left = `${x}px`;
-    coordinate.innerHTML = `${x}px ${y}px`;
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+    document.addEventListener('mousemove', (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+
+        coordinate.innerHTML = `${x + 20}px ${y + 20}px`;
+        coordinate.style.transform = `translate(${x}px, ${y}px)`;
+    });
 });
