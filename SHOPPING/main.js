9d62bb3a83063a1ecd91fc199298b47882f30fc9
신민 <smin_0820@naver.com>
2023-02-14 10:12:12 +0900

    li 항목마다 addEventListener => 이벤트 위임 사용해서 효율적으로 개선

--- SHOPPING/main.js
+++ SHOPPING/main.js
@@ -15,28 +15,19 @@ function onAdd() {
     input.focus();
 };
 
+let id = 0; // UUTD를 사용하는게 더 바람직함
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
-
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText = text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    })
-
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-
-    itemRow.appendChild(item);
+    itemRow.innerHTML = `
+        <div class="item" data-id=${id}>
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="fa-solid fa-trash-can" data-id=${id}></i>
+            </button>
+        </div>
+        `;
+    id++;
     return itemRow;
 };
 
@@ -49,3 +40,11 @@ input.addEventListener('keypress', event => {
         onAdd();
     }
 });
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if (id) {
+        const toBeDeleted = document.querySelector(`.item[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+})
